f1f3f8a9bc614fb5c87e6d4304df7eabd251d9a2
Fred <frederique.perrier@gmail.com>
2025-12-18 17:23:35 +0100

    Add form

--- src/Form/OrderDetailFormType.php
+++ src/Form/OrderDetailFormType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-use App\Entity\OrderDetail;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
