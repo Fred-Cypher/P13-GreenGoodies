bd4a0887eaa5b167f17bbe7b0e5a70ca3ba90216
Fred <frederique.perrier@gmail.com>
2025-12-19 09:10:55 +0100

    Create profile

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('user/profile.html.twig', []);
+    }
+}
