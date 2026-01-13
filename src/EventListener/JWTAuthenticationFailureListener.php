0d77749d9a360f694693a49220dcde24c5719b76
Fred <frederique.perrier@gmail.com>
2026-01-13 13:52:36 +0100

    Add JWTAuthentication and secure API access

---
+++ src/EventListener/JWTAuthenticationFailureListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+
+#[AsEventListener(event: 'lexik_jwt_authentication.on_authentication_failure', method: 'onAuthenticationFailure')]
+class JWTAuthenticationFailureListener
+{
+    public function onAuthenticationFailure(AuthenticationFailureEvent $event): void
+    {
+        $exception = $event->getException();
+//        dd($exception->getMessage());
+
+        if ($exception->getMessage() === 'API_ACCESS_DISABLED')
+        {
+            $response = new JWTAuthenticationFailureResponse(
+                'Accès API non activé pour cet utilisateur.',
+                403
+            );
+            $event->setResponse($response);
+        } elseif (in_array($exception->getMessage(), ['Bad credentials.', 'The presented password is invalid.'])) {
+            $response = new JWTAuthenticationFailureResponse(
+                'Identifiants incorrects, veuillez réessayer.',
+                401
+            );
+            $event->setResponse($response);
+        }
+    }
+}
