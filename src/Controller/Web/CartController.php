fd428d81804a4a193b3947f4b432ccfbd1337e2d
Fred <frederique.perrier@gmail.com>
2026-01-22 18:10:42 +0100

    Add isGranted and correct delete user

--- src/Controller/Web/CartController.php
+++ src/Controller/Web/CartController.php
@@ -11,8 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/cart')]
+#[IsGranted('ROLE_USER')]
 class CartController extends AbstractController
 {
     public function __construct(
--- src/Controller/Web/UserController.php
+++ src/Controller/Web/UserController.php
@@ -11,10 +11,13 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/profile')]
+#[IsGranted('ROLE_USER')]
 class UserController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+    #[Route('/', name: 'app_profile')]
     public function profile(OrderRepository $orderRepository): Response
     {
         $orders = $orderRepository->findBy([
@@ -29,7 +32,7 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/profile/api-access', name: 'app_profile_api_access', methods: ['POST'])]
+    #[Route('/api-access', name: 'app_profile_api_access', methods: ['POST'])]
     public function apiAccess(EntityManagerInterface $em): Response
     {
         $this->getUser()->setApiAccess(!$this->getUser()->isApiAccess());
@@ -40,23 +43,22 @@ class UserController extends AbstractController
         return $this->redirectToRoute('app_profile');
     }
 
-    #[Route('/profile/delete', name: 'app_profile_delete', methods: ['POST'])]
+    #[Route('/delete', name: 'app_profile_delete', methods: ['POST'])]
     public function delete(EntityManagerInterface $em, Request $request, Security $security): Response
     {
         $user = $this->getUser();
-
-        if (!$user instanceof User) {
-            return $this->redirectToRoute('app_login');
-        }
+        /** @var User $user */
 
         if (!$this->isCsrfTokenValid('delete-user', $request->request->get('_token'))) {
             throw $this->createAccessDeniedException();
         }
 
-        $security->logout(false);
-
         $em->remove($user);
         $em->flush();
+
+        $security->logout(false);
+
+        $this->addFlash('success', 'Votre compte a été supprimé avec succès.');
 
         return $this->redirectToRoute('app_home');
     }
