ea5ce18d8edc03ccfa1682847d037f85e03c6b00
Fred <frederique.perrier@gmail.com>
2026-01-22 12:42:30 +0100

    Rename and modify security for cart

--- src/Controller/Web/OrderController.php
+++ src/Controller/Web/OrderController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('order')]
+#[Route('/cart')]
 class OrderController extends AbstractController
 {
     public function __construct(
@@ -20,42 +20,23 @@ class OrderController extends AbstractController
         private readonly EntityManagerInterface $em,
     ){}
 
-    #[Route('/cart', name: 'app_cart_show', methods: ['GET'])]
+    #[Route('/', name: 'app_cart_show', methods: ['GET'])]
     public function cartShow(): Response
     {
-        $user = $this->getUser();
+        $cart = $this->cartService->getCurrentCart($this->getUser());
 
-        if(!$user instanceof \App\Entity\User){
-            return $this->redirectToRoute('app_login');
-        }
-
-        $order = $this->cartService->getCurrentCart($user);
-        if(!$order){
-            return $this->render('order/cart.html.twig', [
-                'order' => $order,
-            ]);
-        }
-        $cartItems = $order->getOrderDetails();
-
-        return $this->render('order/cart.html.twig', [
-            'order' => $order,
-            'cartItems' => $cartItems,
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cart,
         ]);
     }
 
-    #[Route('/cart/add/{id}', name: 'app_add_product', methods: ['POST'])]
+    #[Route('/add/{id}', name: 'app_add_product', methods: ['POST'])]
     public function addOrUpdateProduct(Product $product, Request $request): Response
     {
-        $user = $this->getUser();
-
-        if(!$user instanceof \App\Entity\User){
-            return $this->redirectToRoute('app_login');
-        }
-
         $quantity = max(1, (int) $request->request->get('quantity', 0));
 
-        $order = $this->cartService->getOrCreateCurrentCart($user);
-        $this->cartService->addOrUpdateProduct($order, $product, $quantity);
+        $cart = $this->cartService->getOrCreateCurrentCart($this->getUser());
+        $this->cartService->addOrUpdateProduct($cart, $product, $quantity);
 
         return $this->redirectToRoute('app_cart_show');
     }
@@ -64,23 +45,17 @@ class OrderController extends AbstractController
     /**
      * @throws RandomException
      */
-    #[Route('/cart/validate', name:'app_validate_order', methods: ['POST'])]
+    #[Route('/validate', name:'app_validate_order', methods: ['POST'])]
     public function validateOrder(): Response
     {
-        $user = $this->getUser();
+        $cart = $this->cartService->getOrCreateCurrentCart($this->getUser());
 
-        if(!$user instanceof \App\Entity\User){
-            return $this->redirectToRoute('app_login');
-        }
-
-        $order = $this->cartService->getOrCreateCurrentCart($user);
-
-        if ($order->getStatus() !== OrderStatusEnum::PENDING || $order->getOrderDetails()->isEmpty()) {
+        if ($cart->getStatus() !== OrderStatusEnum::PENDING || $cart->getOrderDetails()->isEmpty()) {
             $this->addFlash('warning', 'Commande invalide ou panier vide');
             return $this->redirectToRoute('app_home');
         }
 
-        $this->cartService->validateOrder($order);
+        $this->cartService->validateOrder($cart);
 
         $this->addFlash('success', 'Votre commande a bien été validée');
 
@@ -88,15 +63,10 @@ class OrderController extends AbstractController
     }
 
 
-    #[Route('/cart/delete', name: 'app_cart_delete', methods: ['POST'])]
+    #[Route('/delete', name: 'app_cart_delete', methods: ['POST'])]
     public function deleteCart(): Response
     {
-        $user = $this->getUser();
-
-        if(!$user instanceof \App\Entity\User){
-            return $this->redirectToRoute('app_login');
-        }
-        $order = $this->cartService->getCurrentCart($user);
+        $order = $this->cartService->getCurrentCart($this->getUser());
 
         if (!$order) {
             return $this->redirectToRoute('app_home');
